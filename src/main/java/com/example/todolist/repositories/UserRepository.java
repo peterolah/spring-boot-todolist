dbbceb4075307f17d0fa03614a8a2fdf91fb060b
Peter Olah2 <peter_olah2@epam.com>
2017-02-14 16:55:10 +0100

    added loggedInAs endpoint

--- src/main/java/com/example/todolist/repositories/UserRepository.java
+++ src/main/java/com/example/todolist/repositories/UserRepository.java
@@ -1,14 +1,16 @@
 package com.example.todolist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.todolist.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByUsername(final String username);
+	
+	@Query("select u from User u where u.id = ?#{principal.id}")
+	User loggedInAs();
 	
 	
 			
